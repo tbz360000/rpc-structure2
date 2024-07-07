c074b2b72f6995ba7ab3e652f3e253616e164c4f
trh <1741174571@qq.com>
2024-07-07 17:49:26 +0800

    another defination of rpc

---
+++ inherit.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class A{
+public:
+   virtual void fun(){};
+};
+
+/*class B : public A{
+    void fun(){
+        cout<<"dsds"<<endl;
+    }
+};*/
+// is testing the result1
+int main(){
+
+    A a;
+    {
+    A b(a);
+    }
+
+
+    
+
+
+    return 0;
+}
